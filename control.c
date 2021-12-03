4b16372e3bec21a62cdb0c7ff362f1fa4b8ec41a
cnelson20 <cjnelson2004@gmail.com>
2021-12-03 17:15:08 +0000

    code

--- control.c
+++ control.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h> 
 #include <sys/ipc.h> 
 #include <sys/sem.h>
+#include <sys/shm.h>
 
 #define SEMKEY 5423508
 #define MEMKEY 4580924
+
+union semun {
+	int val;                  //used for SETVAL
+    struct semid_ds *buf;     //used for IPC_STAT and IPC_SET
+    unsigned short  *array;   //used for SETALL
+    struct seminfo  *__buf;
+};
 
 int main(int argc, char *argv[]) {
 	if (argc < 2) {
@@ -16,12 +25,28 @@ int main(int argc, char *argv[]) {
 	} else {
 		if (!strcmp(argv[1],"-c")) {
 			int semd = semget(SEMKEY,1,IPC_CREAT | IPC_EXCL | 0600);
-			semctl(semd,0,SETVAL,1);
-			int fd = open("telephone.txt",O_WRONLY | O_TRUNC);
+			int memd = shmget(MEMKEY,sizeof(int), IPC_CREAT | IPC_EXCL | 0600);
+	
+			union semun u;
+			u.val = 1;
+			semctl(semd,0,SETVAL,u);
+			int fd = open("telephone.txt",O_CREAT | O_WRONLY | O_TRUNC, 0644);
 			close(fd);
 		} else if (!strcmp(argv[1],"-r")) {
 			int semd = semget(SEMKEY,0,0);
+			int memd = shmget(MEMKEY,0,0);
+			shmctl(memd,IPC_RMID,NULL);
 			semctl(semd,0,IPC_RMID);
+						
+			int fd = open("telephone.txt",O_RDONLY);
+			struct stat stp;
+			stat("telephone.txt",&stp);
+			char *buffer = malloc(stp.st_size + 1);
+			buffer[stp.st_size] = '\0';
+			read(fd,buffer,stp.st_size);
+			printf("The Full Story:\n%s",buffer);
+			free(buffer);
+			close(fd);
 		} else {
 			printf("Invalid flag '%s'\n",argv[1]);
 			return 1;
--- write.c
+++ write.c
@@ -1,3 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h> 
+#include <sys/ipc.h> 
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <errno.h>
+
+#define SEMKEY 5423508
+#define MEMKEY 4580924
+
 int main() {
+	int semd = semget(SEMKEY,0,0);
+	int memd = shmget(MEMKEY,0,0);
+	char input[1024];
+	
+	struct sembuf sbuf = {-1,0,0};
+	semop(semd,&sbuf,1);
+	
+	int *shrdmem = shmat(memd,NULL,0);
+	if (shrdmem == (void *)-1) {
+		printf("%s\n",strerror(errno));
+	}
+	struct stat stp;
+	stat("telephone.txt",&stp);
+	int fd;
+	*shrdmem;
+	printf("if check\n");
+	if (stp.st_size != 0) {
+		fd = open("telephone.txt",O_RDONLY);
+		lseek(fd,*shrdmem * -1,SEEK_END);
+		read(fd,input,*shrdmem);
+		close(fd);
+		printf("Last line: %s",input);
+	} else {
+		printf("You're starting a story!\n");
+	}
+	
+	fgets(input,1024,stdin);
+	fd = open("telephone.txt",O_WRONLY | O_APPEND);
+	write(fd,input,strlen(input));
+	close(fd);
+	
+	*shrdmem = strlen(input);
+	shmdt(shrdmem);
+	
+	sbuf.sem_op = 1; // make it {1,0,0}
+	semop(semd,&sbuf,1);
 	
 }
